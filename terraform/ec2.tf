d198e51a2a0cee7a11502a029219ed89bdd96689
Guilherme Linares <guimine.20@gmail.com>
2025-04-04 16:48:25 -0300

    Fix

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -16,6 +16,7 @@ resource "aws_vpc_ipv4_cidr_block_association" "awsec2app" {
 
 resource "aws_subnet" "awsec2app" {
   vpc_id = var.aws_vpc_id
+  availability_zone = "sa-east-1a"
   cidr_block = "172.31.48.0/20"
   map_public_ip_on_launch = true
 
