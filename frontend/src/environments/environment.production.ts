ace30a8ae5a8ea4a0d76822351cbb875510c371b
Guilherme Linares <guimine.20@gmail.com>
2025-04-17 00:50:04 -0300

    Adjust

---
+++ frontend/src/environments/environment.production.ts
@@ -0,0 +1,14 @@
+import { EnvService } from "../app/services/env.service";
+
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: EnvService.apiKeyFirebase,
+        authDomain: "dormitorio-bras.firebaseapp.com",
+        projectId: "dormitorio-bras",
+        storageBucket: "dormitorio-bras.firebasestorage.app",
+        messagingSenderId: "774952298886",
+        appId: "1:774952298886:web:664aa17340ee841e4ea407",
+        measurementId: "G-3Y2NEHPLKV"
+    }
+  };
